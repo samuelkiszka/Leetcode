38e316ef3fa131de20031ab71c3e9aa599276132
Samuel Kiszka <samuel.kiszka26@gmail.com>
2024-07-20 22:01:09 +0200

    Time: 381 ms (85.76%), Space: 22 MB (23.61%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.py
@@ -3,12 +3,27 @@ class Solution:
         rows = len(rowSum)
         cols = len(colSum)
         res = [[0 for _ in range(cols)] for _ in range(rows)]
+
+        i = 0
+        j = 0
+
+        while i < rows and j < cols:
+            res[i][j] = min(rowSum[i], colSum[j])
+            if rowSum[i] == colSum[j]:
+                j += 1
+                i += 1
+            elif rowSum[i] > colSum[j]:
+                rowSum[i] -= colSum[j]
+                j += 1
+            else:
+                colSum[j] -= rowSum[i]
+                i += 1
         
-        for i in range(rows):
-            for j in range(cols):
-                cur = min(rowSum[i], colSum[j])
-                res[i][j] = cur
-                rowSum[i] -= cur
-                colSum[j] -= cur
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         cur = min(rowSum[i], colSum[j])
+        #         res[i][j] = cur
+        #         rowSum[i] -= cur
+        #         colSum[j] -= cur
 
         return res
